cf65602a8535b6b408ed06e95b805fce43d235a4
TanayB11 <tanaybiradar24@gmail.com>
2020-06-20 12:48:08 -0700

    Init vuetify
    Co-authored-by: Riley Kong <kongriley@gmail.com>
    Co-authored-by: Siddharth Nath <37987542+SidNath21@users.noreply.github.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-layout column justify-center align-center>
+    hello world
+	</v-layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
